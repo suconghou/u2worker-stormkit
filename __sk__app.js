25f57cc72320cb03bb3add9d9adbd54485290ae9
suconghou <1126045770@qq.com>
2021-10-11 21:09:45 +0800

    fix: empty check

--- __sk__app.js
+++ __sk__app.js
@@ -501,7 +501,7 @@ class infoParser {
             info['error'] = this.error;
             return info;
         }
-        for (let item of this.streamingData.formats) {
+        for (let item of (this.streamingData.formats || [])) {
             const itag = String(item.itag);
             const s = {
                 "quality": item.qualityLabel || item.quality,
@@ -512,7 +512,7 @@ class infoParser {
             };
             streams[itag] = s;
         }
-        for (let item of this.streamingData.adaptiveFormats) {
+        for (let item of (this.streamingData.adaptiveFormats || [])) {
             const itag = String(item.itag);
             const s = {
                 "quality": item.qualityLabel || item.quality,
